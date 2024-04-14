5042d8f717793eff325152ec9984098469d96fb5
rustyff12 <rustyff123@gmail.com>
2024-04-14 02:54:49 +0200

    mobile nav, extra images added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-// Main js handlers
+const hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", () => {
+    const navBar = document.querySelector(".navbar-mobile");
+    const navBarUl = document.querySelector(".navbar-mobile ul");
+    if (!navBar.classList.contains("show")) {
+        navBar.classList.add("show");
+        setTimeout(() => {
+            navBarUl.style.display = "flex";
+        }, 300);
+    } else {
+        navBar.classList.remove("show");
+        setTimeout(() => {
+            navBarUl.style.display = "none";
+        }, 300);
+    }
+});
+
+// <!-- <i class="fa-solid fa-x"></i> -->
